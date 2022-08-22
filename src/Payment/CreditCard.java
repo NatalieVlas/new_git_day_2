bf5b3a1f41582580d55c29cf1bd2e7098246a7a5
Natalia <nata.vlasiuk17@gmail.com>
2022-08-21 22:02:52 -0500

    comment 3

--- src/Payment/CreditCard.java
+++ src/Payment/CreditCard.java
@@ -4,5 +4,6 @@ public class CreditCard {
     public static void main(String[] args) {
         System.out.println(" Credit Card ; visa");
         System.out.println("new Card");
+        // comment
     }
 }
