b0f0a8c894eef7d99529adb9704fcb618ae70272
Natalia <nata.vlasiuk17@gmail.com>
2022-08-21 22:02:24 -0500

    credit card created 2

--- src/Payment/CreditCard.java
+++ src/Payment/CreditCard.java
@@ -3,5 +3,6 @@ package Payment;
 public class CreditCard {
     public static void main(String[] args) {
         System.out.println(" Credit Card ; visa");
+        System.out.println("new Card");
     }
 }
